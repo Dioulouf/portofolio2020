539d0b79ee5e39185cc44324c23778475ac2a154
Clément <clementdiouloufet0799@gmail.com>
2020-04-06 09:00:32 +0200

    add header and description section

--- sources/scripts/script.js
+++ sources/scripts/script.js
@@ -1,1 +1,52 @@
-console.log("test");
+// **********************************      Variables     *******************************
+
+
+// **********************************      Functions    *******************************
+
+
+function parralax(element, distance, speed) {
+    const item = document.querySelector(element);
+    const ratio = distance * speed;
+    item.style.transform = "translateY(" + ratio + "px)";
+}
+
+
+
+
+
+function revealAnimation(target, ratio, classAdd) {
+    let Target = document.querySelector(target);
+    var options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: ratio
+    };
+
+    function handleIntersect(entries, observer) {
+        entries.forEach(function (entry) {
+            if (entry.intersectionRatio > ratio) {
+                entry.target.classList.add(classAdd)
+                observer.unobserve(entry.target)
+            }
+        });
+    }
+
+    const observer = new IntersectionObserver(handleIntersect, options);
+
+    document.querySelectorAll('[class*="reveal-"]').forEach(function (r) {
+        observer.observe(r);
+    })
+
+    observer.observe(Target);
+}
+
+
+
+// **********************************      IHM     *******************************
+
+
+revealAnimation(".reveal", .5, 'reveal-visible')
+
+window.addEventListener('scroll', function () {
+    parralax('.section--description p', window.scrollY, -0.15);
+});
